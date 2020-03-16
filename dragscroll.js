985428a660b40dd647047b030eb71a180e953349
Haoyang Wei <weihaoyang@ainnovation.com>
2020-03-16 17:52:45 +0800

    add zoom throttle

--- dragscroll.js
+++ dragscroll.js
@@ -22,12 +22,12 @@
     var mouseup = 'mouseup';
     var mousedown = 'mousedown';
     var EventListener = 'EventListener';
-    var addEventListener = 'add'+EventListener;
-    var removeEventListener = 'remove'+EventListener;
+    var addEventListener = 'add' + EventListener;
+    var removeEventListener = 'remove' + EventListener;
     var newScrollX, newScrollY;
 
     var dragged = [];
-    var reset = function(i, el) {
+    var reset = function (i, el) {
         for (i = 0; i < dragged.length;) {
             el = dragged[i++];
             el = el.container || el;
@@ -39,10 +39,10 @@
         // cloning into array since HTMLCollection is updated dynamically
         dragged = [].slice.call(_document.getElementsByClassName('dragscroll'));
         for (i = 0; i < dragged.length;) {
-            (function(el, lastClientX, lastClientY, pushed, scroller, cont){
+            (function (el, lastClientX, lastClientY, pushed, scroller, cont) {
                 (cont = el.container || el)[addEventListener](
                     mousedown,
-                    cont.md = function(e) {
+                    cont.md = function (e) {
                         if (!el.hasAttribute('nochilddrag') ||
                             _document.elementFromPoint(
                                 e.pageX, e.pageY
@@ -58,17 +58,17 @@
                 );
 
                 _window[addEventListener](
-                    mouseup, cont.mu = function() {pushed = 0;}, 0
+                    mouseup, cont.mu = function () { pushed = 0; }, 0
                 );
 
                 _window[addEventListener](
                     mousemove,
-                    cont.mm = function(e) {
+                    cont.mm = function (e) {
                         if (pushed) {
-                            (scroller = el.scroller||el).scrollLeft -=
-                                newScrollX = (- lastClientX + (lastClientX=e.clientX));
+                            (scroller = el.scroller || el).scrollLeft -=
+                                newScrollX = (- lastClientX + (lastClientX = e.clientX));
                             scroller.scrollTop -=
-                                newScrollY = (- lastClientY + (lastClientY=e.clientY));
+                                newScrollY = (- lastClientY + (lastClientY = e.clientY));
                             if (el == _document.body) {
                                 (scroller = _document.documentElement).scrollLeft -= newScrollX;
                                 scroller.scrollTop -= newScrollY;
@@ -76,11 +76,11 @@
                         }
                     }, 0
                 );
-             })(dragged[i++]);
+            })(dragged[i++]);
         }
     }
 
-      
+
     if (_document.readyState == 'complete') {
         reset();
     } else {
--- zoom.js
+++ zoom.js
@@ -1,20 +1,22 @@
-window.onload = function(){
+window.onload = function () {
     window.scrollTo(875, 625);
 };
-window.onbeforeprint = function(){
+window.onbeforeprint = function () {
     window.scrollTo(875, 625);
 };
-const vis= document.querySelector('#vision');
-let scale = 1, lastScale = 1;
-vis.addEventListener('mousewheel', function(e){
-    if(e.wheelDelta>0){
+const vis = document.querySelector('#vision');
+let scale = 1, lastScale = 1, lastTimeStamp = 0;
+vis.addEventListener('mousewheel', function (e) {
+    if (Date.now() - lastTimeStamp < 75) return;
+    lastTimeStamp = Date.now();
+    if (e.wheelDelta > 0) {
         scale += 0.2;
         scale = Math.min(2.6, scale);
-    }else if(e.wheelDelta<0){
+    } else if (e.wheelDelta < 0) {
         scale -= 0.2;
         scale = Math.max(scale, 0.2);
     }
-    if(scale === lastScale) return;
+    if (scale === lastScale) return;
     lastScale = scale;
     this.style.transform = `scale(${scale})`;
 })
